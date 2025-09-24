01550dccfd60b0519c7479d636ce887210a533da
Ujjwal shukla <shuklaujjwal6260@gmail.com>
2025-09-25 01:46:40 +0530

    cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -10,18 +9,27 @@ import chatRoutes from "./routes/chat.route.js";
 
 import { connectDB } from "./lib/db.js";
 
+const allowedOrigins = [
+  "http://localhost:5173", // local dev
+  "https://friends-app-chi.vercel.app", // your deployed frontend
+];
+
 const app = express();
 const PORT = process.env.PORT;
 
-const __dirname = path.resolve();
-
 app.use(
   cors({
-    origin:"https://friends-app-chi.vercel.app",
-    credentials: true,
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // needed for cookies
   })
 );
-
 
 app.use(express.json());
 app.use(cookieParser());
@@ -29,7 +37,6 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
