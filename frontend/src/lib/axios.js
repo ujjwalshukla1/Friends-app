2945af4d89d5f1cc224630eee13e6647918c87b7
Ujjwal shukla <shuklaujjwal6260@gmail.com>
2025-09-25 01:49:39 +0530

    /api-added

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://friends-app-1-62ci.onrender.com",
+  baseURL: "https://friends-app-1-62ci.onrender.com/api",
   withCredentials: true, // send cookies with the request
 });
