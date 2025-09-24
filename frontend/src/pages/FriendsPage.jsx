55c2ba192609025bce6cf7e686f57e9b14f4a324
Ujjwal shukla <shuklaujjwal6260@gmail.com>
2025-09-24 23:23:33 +0530

    firends-page-added-with-themes

---
+++ frontend/src/pages/FriendsPage.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { getUserFriends } from "../lib/api";
+import { useQuery } from "@tanstack/react-query";
+import FriendCard from "../components/FriendCard";
+
+const FriendsPage = () => {
+  const { data: friends = [], isLoading: loadingFriends } = useQuery({
+    queryKey: ["friends"],
+    queryFn: getUserFriends,
+  });
+
+  return (
+    <div className="p-4 sm:p-6 lg:p-8">
+      {loadingFriends ? (
+        <div className="flex justify-center py-12">
+          <span className="loading loading-spinner loading-lg" />
+        </div>
+      ) : friends.length === 0 ? (
+        <NoFriendsFound />
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          {friends.map((friend) => (
+            <FriendCard key={friend._id} friend={friend} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FriendsPage;
